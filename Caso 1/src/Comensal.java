69f7ba3c12d6dca232d84d7aac02f458361ca247
unknown <ea.nielsen@uniandes.edu.co>
2021-08-28 20:38:40 -0500

    comprension caso 1.1

---
+++ Caso 1/src/Comensal.java
@@ -0,0 +1,32 @@
+
+public class Comensal extends Thread{
+	
+private int numPlatos;
+private boolean tieneCubierto1 = false;
+private boolean tieneCubierto2 = false;
+
+
+	
+	
+public void comer(){
+	while(numPlatos != 0){
+		int x = 1+x;
+		recogerCubiertos()
+		comer()
+		if(numPlatos == x)
+		{
+			wait()
+		}
+		dejarCubiertosregadero()
+	}
+
+}
+
+
+
+public void soltar(){
+	if(tieneCubierto1== true && tieneCubierto2== false) {
+		
+	}
+}
+}
---
+++ Caso 1/src/Lavaplatos.java
@@ -0,0 +1,5 @@
+
+public class Lavaplatos extends Thread{
+	
+
+}
