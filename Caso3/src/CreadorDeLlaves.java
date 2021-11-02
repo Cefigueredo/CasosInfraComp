ab55175e5075285e22be503dbccf0507020a817b
Carlos Figueredo <cefigueredo@uniandes.edu.co>
2021-11-01 22:23:25 -0500

    Iniciando creador de llaves

---
+++ Caso3/src/CreadorDeLlaves.java
@@ -0,0 +1,36 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+/**
+ * Fuente: https://www.tutorialspoint.com/java_cryptography/java_cryptography_storing_keys.htm
+ * @author Carlos
+ *
+ */
+public class CreadorDeLlaves {
+
+
+	private final static String PADDING = "AES/ECB/PKCS5Padding";
+	private final static String ALGORITMO = "AES";
+	public static void main(String[] args) throws NoSuchAlgorithmException, CertificateException, IOException {
+		KeyGenerator keygen = KeyGenerator.getInstance(ALGORITMO);
+		SecretKey secretKey = keygen.generateKey();
+		System.out.println("llave creada: "+secretKey.toString());
+		FileOutputStream archivo = new FileOutputStream("llaveSimetrica");
+		ObjectOutputStream oos = new ObjectOutputStream(archivo);
+		
+		oos.writeObject(secretKey);
+		oos.close();
+		System.out.println("Llave guardada");
+	}
+	
+}
--- Caso3/src/Main.java
+++ Caso3/src/Main.java
@@ -1,14 +1,50 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Scanner;
+
+import javax.crypto.SecretKey;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+		
 		//Pide cantidad de clientes y si usa cifrado simétrico o asimétrico
+		Scanner sc=new Scanner(System.in);  
+		
+		
+		System.out.println("Ingrese la cantidad de clientes: ");
+		int cantidadClientes = sc.nextInt();
+		System.out.println("Usted eligió "+cantidadClientes+" clientes.");
+		
+		System.out.println("Ingrese 1 si quiere usar cifrado simétrico o 2 si quiere usar cifrado asimétrico: ");
+		int nTipoCifrado = sc.nextInt();
+		
+		
+		while(nTipoCifrado!=1 && nTipoCifrado!=2) {
+			System.out.println("Error, digite nuevamente: ");
+			nTipoCifrado = sc.nextInt();
+		}
+		
+		if(nTipoCifrado==1) {
+			System.out.println("Usted eligió cifrado simétrico");
+		}
+		else if(nTipoCifrado==2) {
+			System.out.println("Usted eligió cifrado asimétrico");
+		}
+		else {
+			System.out.println("Error");
+		}
 		
 		
 		
-		
-		//Crea las llaves (O ya están creadas?)
-		
-		
+		//Toma las llaves de los archivos
+		FileInputStream archivo = new FileInputStream("llaveSimetrica");
+		ObjectInputStream ois = new ObjectInputStream(archivo);
+		SecretKey sk = (SecretKey) ois.readObject();
+		ois.close();
+		System.out.println("Algoritmo de la llave es: "+sk.getAlgorithm());
 		
 		
 		//Ejecuta 
